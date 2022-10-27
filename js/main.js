f463c6aa96c10822c99c94cc532b571444c0f680
TomasBelfonte <tomas.belfonte@gmail.com>
2022-10-27 20:57:31 +0200

    JS-Biglietto_treno

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const inputKm = prompt ("Inserire il numero di Km Da percorrere");
+
+if (isNaN(inputKm)) {
+    alert("il valore inserito non è valido")
+}
+
+const inputAge = prompt ("inserire l'età del passeggero");
+
+if (isNaN(inputAge)) {
+    alert("il valore inserito non è valido");
+}
+
+if (inputAge >=18 && inputAge <=65) {
+    function tbfunction() {
+    var num = (inputKm * 0.21);
+    var n = num.toFixed(2)
+    document.getElementById("costoBiglietto").innerHTML = n;
+    }
+}
+
+if (inputAge <18) {
+    function tbfunction() {
+    var num = ((inputKm * 0.21) * 0.8);
+    var n = num.toFixed(2)
+    document.getElementById("costoBiglietto").innerHTML = n;
+    }
+}
+
+if (inputAge >65) {
+    function tbfunction() {
+    var num = ((inputKm * 0.21) * 0.6);
+    var n = num.toFixed(2)
+    document.getElementById("costoBiglietto").innerHTML = n;
+    }
+}
